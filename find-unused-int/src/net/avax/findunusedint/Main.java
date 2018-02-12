14d1d905efeea5875d57e33ccc43ec993ccd1172
Michael Krueger <michael.krueger@windriver.com>
2018-02-12 14:58:50 -0800

    Clean up Java integer finder

--- find-unused-int/src/net/avax/findunusedint/Main.java
+++ find-unused-int/src/net/avax/findunusedint/Main.java
@@ -3,17 +3,33 @@ package net.avax.findunusedint;
 import java.util.Scanner;
 
 public class Main {
+    private static void recordUsedInt(int usedInt) {
+    }
+
+    private static Integer findLowestUnusedInt() {
+        Integer result = null;
+
+        return result;
+    }
 
     public static void main(String[] args) {
+        long count = 0;
+
         Scanner sc = new Scanner(System.in);
 
-        int count = 0;
-
         while (sc.hasNextInt()) {
-            int i = sc.nextInt();
+            recordUsedInt(sc.nextInt());
             count++;
         }
 
         System.out.println("Read " + count + " integer(s).");
+
+        Integer result = findLowestUnusedInt();
+
+        if (result == null) {
+            System.out.println("All possible integer values have been used.");
+        } else {
+            System.out.println("The lowest unused integer is " + result + ".");
+        }
     }
 }
