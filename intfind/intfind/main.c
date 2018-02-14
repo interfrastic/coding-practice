038790a98e3b649c0c2a5d57caa851c9cff2e747
Michael Krueger <interfrastic@users.noreply.github.com>
2018-02-14 11:31:54 -0800

    Refine C integer finder based on original problem

--- intfind/intfind/main.c
+++ intfind/intfind/main.c
@@ -6,6 +6,15 @@
 //  Copyright © 2018 Michael J. Krueger. All rights reserved.
 //
 
+// From Programming Pearls, Second Edition, by Jon Bentley, Addison-Wesley, Inc.,
+// 2000, ISBN 0-201-65788-0:
+//
+// Given a sequential file that contains at most four billion integers in random
+// order, find a 32-bit integer that isn't in the file (and there must be at
+// least one missing--why?).  How would you solve this problem with ample
+// quantities of main memory? How would you solve it if you could use several
+// external "scratch" files but only a few hundred bytes of main memory?
+
 #include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -14,27 +23,28 @@
 typedef unsigned short bin_t;
 
 #define BITS_PER_BIN    (sizeof (bin_t) * CHAR_BIT)
-#define BINS_SIZE       ((INT_MAX / BITS_PER_BIN) + 1)
+#define BINS_SIZE       ((((unsigned long) INT_MAX - (unsigned long) INT_MIN) / BITS_PER_BIN) + 1)
 
 static void record_used_int(bin_t *bins, int n) {
-    int bin_index = n / BITS_PER_BIN;
-    bin_t bin_mask = 1ul << (n % BITS_PER_BIN);
+    unsigned long offset_n = (unsigned long) n - (unsigned long) INT_MIN;
+    unsigned long bin_index = offset_n / BITS_PER_BIN;
+    bin_t bin_mask = 1ul << (offset_n % BITS_PER_BIN);
 
     bins[bin_index] |= bin_mask;
 }
 
-static int find_lowest_unused_int(bin_t *bins) {
-    for (int n = 0, bin_index = 0; bin_index < BINS_SIZE; bin_index++) {
-        for (int bit = 0; bit < BITS_PER_BIN; bit++, n++) {
-            bin_t bin_mask = 1ul << bit;
+static int find_unused_int(bin_t *bins) {
+    for (int n = INT_MIN; n < INT_MAX; n++) {
+        unsigned long offset_n = (unsigned long) n - (unsigned long) INT_MIN;
+        unsigned long bin_index = offset_n / BITS_PER_BIN;
+        bin_t bin_mask = 1ul << (offset_n % BITS_PER_BIN);
 
-            if ((bins[bin_index] & bin_mask) == 0) {
-                return n;
-            }
+        if ((bins[bin_index] & bin_mask) == 0) {
+            return n;
         }
     }
 
-    return -1;
+    return INT_MAX;
 }
 
 int main(int argc, const char * argv[]) {
@@ -42,20 +52,16 @@ int main(int argc, const char * argv[]) {
     int n;
     long count = 0;
 
-    while (scanf("%d", &n) == 1 && n >= 0) {
+    while (scanf("%d", &n) == 1 && count < 4000000000) {
         record_used_int(bins, n);
         count++;
     }
 
-    printf("Read %ld non-negative integer(s).\n", count);
+    printf("Read %ld integer(s).\n", count);
 
-    int found = find_lowest_unused_int(bins);
+    n = find_unused_int(bins);
 
-    if (found >= 0) {
-        printf("The lowest unused non-negative integer is %d.\n", found);
-    } else {
-        printf("All possible non-negative integer values have been used.\n");
-    }
+    printf("%d is not in the file.\n", n);
 
     free(bins);
 
