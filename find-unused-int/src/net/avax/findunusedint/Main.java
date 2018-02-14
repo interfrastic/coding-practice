effe8de567cdfc57c03fe76492e73727dd42aca5
Michael Krueger <interfrastic@users.noreply.github.com>
2018-02-14 11:30:27 -0800

    Rewrite Java integer finder using BitSet

--- find-unused-int/src/net/avax/findunusedint/Main.java
+++ find-unused-int/src/net/avax/findunusedint/Main.java
@@ -1,34 +1,22 @@
 package net.avax.findunusedint;
 
+import java.util.BitSet;
 import java.util.Scanner;
 
 public class Main {
     private static final int BITS_PER_BIN = Character.BYTES * Byte.SIZE;
     private static final int BINS_SIZE = (Integer.MAX_VALUE / BITS_PER_BIN) + 1;
 
-    private static void recordUsedInt(char[] bins, int n) {
-        int bin_index = n / BITS_PER_BIN;
-        char bin_mask = (char) (1 << (n % BITS_PER_BIN));
-
-        bins[bin_index] |= bin_mask;
+    private static void recordUsedInt(BitSet bins, int n) {
+        bins.set(n);
     }
 
-    private static int findLowestUnusedInt(char[] bins) {
-        for (int n = 0, bin_index = 0; bin_index < BINS_SIZE; bin_index++) {
-            for (int bit = 0; bit < BITS_PER_BIN; bit++, n++) {
-                char bin_mask = (char) (1 << bit);
-
-                if ((bins[bin_index] & bin_mask) == 0) {
-                    return n;
-                }
-            }
-        }
-
-        return -1;
+    private static int findLowestUnusedInt(BitSet bins) {
+        return bins.nextClearBit(0);
     }
 
     public static void main(String[] args) {
-        char bins[] = new char[BINS_SIZE];
+        BitSet bins = new BitSet();
         int n;
         long count = 0;
 
