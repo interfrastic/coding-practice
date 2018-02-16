dd5eb9c138df2be559353dde87b64306735eb82b
Michael Krueger <michael.krueger@windriver.com>
2018-02-16 11:37:25 -0800

    Reduce Java integer finder memory consumption

--- find-unused-int/src/net/avax/findunusedint/Main.java
+++ find-unused-int/src/net/avax/findunusedint/Main.java
@@ -1,5 +1,17 @@
 package net.avax.findunusedint;
 
+// From Programming Pearls, Second Edition, by Jon Bentley, Addison-Wesley,
+// Inc., 2000, ISBN 0-201-65788-0:
+//
+// Given a sequential file that contains at most four billion integers in
+// random order, find a 32-bit integer that isn't in the file (and there must
+// be at least one missing--why?).  How would you solve this problem with ample
+// quantities of main memory? How would you solve it if you could use several
+// external "scratch" files but only a few hundred bytes of main memory?
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.List;
@@ -9,34 +21,77 @@ public class Main {
     private static final long COUNT_MAX = 4000000000L;
     private static final int INPUT_BITS = 32;
     private static final long INPUT_SIZE = 1L << INPUT_BITS;
-    private static final int INPUT_BITS_PER_BIT_SET = 31;
+    private static final int INPUT_BITS_PER_BIT_SET = 28;
     private static final long BIT_SET_SIZE = 1L << INPUT_BITS_PER_BIT_SET;
     private static final int BIT_SET_COUNT = (int) (INPUT_SIZE / BIT_SET_SIZE);
 
-    private static void recordUsedInt(List<BitSet> bitSets, long n) {
+    private static BitSet activeBitSet = null;
+    private static int activeBitSetNum = -1;
+    private static List<Path> bitSetPaths = null;
+
+    private static void saveActiveBitSet() throws IOException {
+        try (ObjectOutputStream oos = new ObjectOutputStream(
+                new FileOutputStream(
+                        bitSetPaths.get(activeBitSetNum).toFile()))) {
+            oos.writeObject(activeBitSet);
+        }
+    }
+
+    private static void activateBitSet(int bitSetNum) throws
+            IOException, ClassNotFoundException {
+        if (bitSetNum == activeBitSetNum) {
+            return;
+        }
+
+        saveActiveBitSet();
+
+        try (ObjectInputStream ois = new ObjectInputStream(
+                new FileInputStream(
+                        bitSetPaths.get(bitSetNum).toFile()))) {
+            activeBitSet = (BitSet) ois.readObject();
+        }
+
+        activeBitSetNum = bitSetNum;
+    }
+
+    private static void recordUsedInt(long n) throws IOException,
+            ClassNotFoundException {
         int bitSetNum = (int) (n / BIT_SET_SIZE);
         int bitNum = (int) (n % BIT_SET_SIZE);
 
-        bitSets.get(bitSetNum).set(bitNum);
+        activateBitSet(bitSetNum);
+        activeBitSet.set(bitNum);
     }
 
-    private static long findLowestUnusedInt(List<BitSet> bitSets) {
-        for (int bitSetIndex = 0; bitSetIndex < bitSets.size(); bitSetIndex++) {
-            int clearBit = bitSets.get(bitSetIndex).nextClearBit(0);
+    private static long findLowestUnusedInt() throws IOException,
+            ClassNotFoundException {
+        for (int bitSetNum = 0; bitSetNum < bitSetPaths.size(); bitSetNum++) {
+            activateBitSet(bitSetNum);
+
+            int clearBit = activeBitSet.nextClearBit(0);
 
             if (clearBit >= 0) {
-                return bitSetIndex * BIT_SET_SIZE + clearBit;
+                return bitSetNum * BIT_SET_SIZE + clearBit;
             }
         }
 
         return -1;
     }
 
-    public static void main(String[] args) {
-        List<BitSet> bitSets = new ArrayList<>();
+    public static void main(String[] args) throws IOException,
+            ClassNotFoundException {
+        bitSetPaths = new ArrayList<>();
 
-        for (int i = 0; i < BIT_SET_COUNT; i++) {
-            bitSets.add(new BitSet());
+        for (int bitSetNum = 0; bitSetNum < BIT_SET_COUNT; bitSetNum++) {
+            Path path = Files.createTempFile("find-unused-int-", ".tmp");
+
+            path.toFile().deleteOnExit();
+            bitSetPaths.add(path);
+
+            activeBitSet = new BitSet();
+            activeBitSetNum = bitSetNum;
+
+            saveActiveBitSet();
         }
 
         long n;
@@ -46,14 +101,14 @@ public class Main {
 
         while (sc.hasNextLong() && (n = sc.nextLong()) >= 0 && n < INPUT_SIZE
                 && count < COUNT_MAX) {
-            recordUsedInt(bitSets, n);
+            recordUsedInt(n);
             count++;
         }
 
         System.out.println("Read " + count + " " + INPUT_BITS
                 + "-bit unsigned integer(s).");
 
-        n = findLowestUnusedInt(bitSets);
+        n = findLowestUnusedInt();
 
         System.out.println(n + " is the lowest unused integer.");
     }
