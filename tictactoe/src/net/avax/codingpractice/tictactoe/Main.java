e2cbf83324aa2799714af3790b960d1178e3d7b9
Michael Krueger <5394172+interfrastic@users.noreply.github.com>
2018-03-26 14:29:57 -0700

    Increase default tictactoe board size to 4

--- tictactoe/src/net/avax/codingpractice/tictactoe/Main.java
+++ tictactoe/src/net/avax/codingpractice/tictactoe/Main.java
@@ -100,7 +100,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        int size = 3;
+        int size = 4;
         int limit = 10;
 
         long start = System.currentTimeMillis();
