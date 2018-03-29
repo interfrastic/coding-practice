91d98e20ab638ab0e61e8364c33dd10f7988ea47
Michael Krueger <michael.krueger@windriver.com>
2018-03-29 15:01:55 -0700

    Start playing with camelcaps

--- camelcaps/src/net/avax/codingpractice/camelcaps/Main.java
+++ camelcaps/src/net/avax/codingpractice/camelcaps/Main.java
@@ -1,8 +1,47 @@
 package net.avax.codingpractice.camelcaps;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
+    private static @NotNull String capitalize(@NotNull String word) {
+        if (word.length() == 0) {
+            return word;
+        }
+
+        return word.substring(0, 1).toUpperCase()
+                + word.substring(1).toLowerCase();
+    }
+
+    private static @NotNull String getLowerCamelCaps(
+            @NotNull List<String> words) {
+        int size = words.size();
+
+        if (size == 0) {
+            return "";
+        }
+
+        return words.get(0) + getUpperCamelCaps(words.subList(1, size));
+    }
+
+    private static @NotNull String getUpperCamelCaps(
+            @NotNull List<String> words) {
+        return words.stream()
+                .map(Main::capitalize).collect(Collectors.joining());
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(getLowerCamelCaps(List.of()));
+        System.out.println(getLowerCamelCaps(List.of("foo", "bar", "baz")));
+        System.out.println(getLowerCamelCaps(List.of("one")));
+        System.out.println(getLowerCamelCaps(List.of("one", "", "three")));
+        System.out.println(getLowerCamelCaps(List.of("one", "2", "three")));
+        System.out.println(getUpperCamelCaps(List.of()));
+        System.out.println(getUpperCamelCaps(List.of("foo", "bar", "baz")));
+        System.out.println(getUpperCamelCaps(List.of("one")));
+        System.out.println(getUpperCamelCaps(List.of("one", "", "three")));
+        System.out.println(getUpperCamelCaps(List.of("one", "2", "three")));
     }
 }
