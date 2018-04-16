9884909890c5323cc4450862ede2d30941a0d105
Michael Krueger <michael.krueger@windriver.com>
2018-04-16 15:38:51 -0700

    Create skeleton nqueens project

---
+++ nqueens/src/net/avax/codingpractice/nqueens/Main.java
@@ -0,0 +1,9 @@
+package net.avax.codingpractice.nqueens;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.solveNQueens(4));
+    }
+}
---
+++ nqueens/src/net/avax/codingpractice/nqueens/Solution.java
@@ -0,0 +1,16 @@
+package net.avax.codingpractice.nqueens;
+
+// 2018-04-16 techlet problem:
+//
+// https://leetcode.com/problems/n-queens/description/
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> boards = new ArrayList<>();
+
+        return boards;
+    }
+}
