b1d182d97b2763bb0223c02888f60f9d7e996f80
Michael Krueger <michael.krueger@windriver.com>
2018-04-25 12:01:10 -0700

    use scratchpad for local-variable type inference

--- scratchpad/src/net/avax/scratchpad/Main.java
+++ scratchpad/src/net/avax/scratchpad/Main.java
@@ -1,7 +1,53 @@
 package net.avax.scratchpad;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+// Keynotes: Oracle Code Chicago: Alex Buckley: Local-Variable Type Inference
+//
+// https://www.youtube.com/watch?v=84mCmmzksGI&t=1h15m55s
+//
+// JEP 286: Local-Variable Type Inference
+//
+// http://openjdk.java.net/jeps/286
+//
+// Style Guidelines for Local Variable Type Inference in Java
+//
+// http://openjdk.java.net/projects/amber/LVTIstyle.html
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, scratchpad!");
+    public static void main(String[] args) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new
+                ByteArrayOutputStream(12);
+
+        while (byteArrayOutputStream.size() <= 10) {
+            byteArrayOutputStream.write("hello".getBytes());
+        }
+
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+
+        System.out.println("Displaying byte array created from stream:");
+
+        for (int i = 0; i < bytes.length; i++) {
+            System.out.print((char) bytes[i] + " ");
+        }
+        System.out.println();
+
+        int nextByte;
+        ByteArrayInputStream byteArrayInputStream
+                = new ByteArrayInputStream(bytes);
+
+        System.out.println("Reading and transforming stream of byte array:");
+
+        for (int i = 0; i < 3; i++) {
+            while ((nextByte = byteArrayInputStream.read()) >= 0) {
+                System.out.print(Character.toUpperCase((char) (nextByte + i))
+                        + " ");
+            }
+            System.out.println();
+
+            byteArrayInputStream.reset();
+        }
     }
 }
