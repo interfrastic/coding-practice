823b0479e71e7c1f6069c18af5daf4bf981ab92b
Michael Krueger <5394172+interfrastic@users.noreply.github.com>
2018-12-05 11:13:36 -0800

    Create posttransition Xcode project

---
+++ posttransition/posttransition/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  posttransition
+//
+//  Created by Michael Krueger on 12/4/18.
+//  Copyright © 2018 Michael Krueger. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
