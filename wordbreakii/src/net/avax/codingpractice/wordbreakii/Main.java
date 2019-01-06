3ed54b0b803f61ec8f06dd2cbb5fcca13be70b19
Michael Krueger <5394172+interfrastic@users.noreply.github.com>
2019-01-05 21:18:36 -0800

    Record failed wordbreakii backtracking attempt

--- wordbreakii/src/net/avax/codingpractice/wordbreakii/Main.java
+++ wordbreakii/src/net/avax/codingpractice/wordbreakii/Main.java
@@ -1,5 +1,6 @@
 package net.avax.codingpractice.wordbreakii;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -7,10 +8,20 @@ import java.util.stream.Stream;
 public class Main {
 
     public static void main(String[] args) {
+        // 14 s with original brute-force implementation.
+        // 70 s with first backtracking implementation.
+        String sentence = "aaaaaaaaaaaaaaaaaaaaaaaa";
+        List<String> dictWords = Stream.of("a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa").collect(Collectors.toList());
 //        String sentence = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 //        List<String> dictWords = Stream.of("a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa").collect(Collectors.toList());
-        String sentence = "foobar";
-        List<String> dictWords = Stream.of("foo","bar","foobar").collect(Collectors.toList());
+//        String sentence = "foobar";
+//        List<String> dictWords = Stream.of("foo","bar","foobar").collect(Collectors.toList());
+//        String sentence = "a";
+//        List<String> dictWords = Collections.emptyList();
+//        String sentence = "a";
+//        List<String> dictWords = Stream.of("a").collect(Collectors.toList());
+//        String sentence = "aaaaaaa";
+//        List<String> dictWords = Stream.of("aaaa","aaa").collect(Collectors.toList());
 
         Solution solution = new Solution();
 
--- wordbreakii/src/net/avax/codingpractice/wordbreakii/Solution.java
+++ wordbreakii/src/net/avax/codingpractice/wordbreakii/Solution.java
@@ -1,6 +1,7 @@
 package net.avax.codingpractice.wordbreakii;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @SuppressWarnings("WeakerAccess")
@@ -17,48 +18,146 @@ import java.util.List;
 // Status: Time Limit Exceeded
 //
 // https://leetcode.com/submissions/detail/199216935/
+//
+// Second attempt: optimize to execute within time limit.  Hint: use
+// backtracking.
+//
+// 31 / 39 test cases passed.
+// Status: Time Limit Exceeded
+//
+// https://leetcode.com/submissions/detail/199353852/
 
 class Solution {
-    public List<String> wordBreak(String s, List<String> dictWords) {
+    static class Backtracker {
+        static class Candidate {
+            final public List<Integer> dictWordIndexes;
 
-        // Build a list of possible sentences.
-
-        List<String> sentences = new ArrayList<>();
-
-        // Loop through the words in the dictionary.
-
-        for (String dictWord : dictWords) {
-
-            // If the string starts with the dictionary word, see if there
-            // are any characters left in the string.
-
-            if (s.startsWith(dictWord)) {
-
-                // See what remains in the string.
-
-                String remainder = s.substring(dictWord.length());
-
-                if (remainder.isEmpty()) {
-
-                    // If there are no characters left, then the word itself is
-                    // the sentence; this terminates the recursion.
-
-                    sentences.add(dictWord);
-                } else {
-
-                    // If there are characters left, call this method
-                    // recursively to determine the remaining possible sentence
-                    // fragments, if any. Loop through the remaining fragments,
-                    // combining each with the current dictionary word to form
-                    // a possible sentence.
-
-                    for (String fragment : wordBreak(remainder, dictWords)) {
-                        sentences.add(dictWord + " " + fragment);
-                    }
-                }
+            public Candidate(List<Integer> dictWordIndexes) {
+                this.dictWordIndexes = new ArrayList<>(dictWordIndexes);
             }
         }
 
-        return sentences;
+        final private String s;
+        final private List<String> dictWords;
+        final private int maxDictWordIndex;
+        private List<String> sentences = new ArrayList<>();
+
+        public Backtracker(String s, List<String> dictWords) {
+            this.s = s;
+            this.dictWords = new ArrayList<>(dictWords);
+            maxDictWordIndex = dictWords.size() - 1;
+        }
+
+        private Candidate root() {
+            return new Candidate(Collections.emptyList());
+        }
+
+        private boolean reject(Candidate c) {
+            int maxCharacterCount = s.length();
+            String remainder = s;
+            int characterCount = 0;
+
+            for (int dictWordIndex : c.dictWordIndexes) {
+                String dictWord = dictWords.get(dictWordIndex);
+
+                characterCount += dictWord.length();
+
+                if (characterCount > maxCharacterCount
+                        || !remainder.startsWith(dictWord)) {
+                    return true;
+                }
+
+                remainder = remainder.substring(dictWord.length());
+            }
+
+            return false;
+        }
+
+        private boolean accept(Candidate c) {
+            int characterCount = 0;
+
+            for (int dictWordIndex : c.dictWordIndexes) {
+                String dictWord = dictWords.get(dictWordIndex);
+
+                characterCount += dictWord.length();
+            }
+
+            return characterCount == s.length();
+        }
+
+        private Candidate first(Candidate c) {
+            if (maxDictWordIndex < 0) {
+                return null;
+            }
+
+            List<Integer> dictWordIndexes = new ArrayList<>(c.dictWordIndexes);
+
+            dictWordIndexes.add(0);
+
+            return new Candidate(dictWordIndexes);
+        }
+
+        private Candidate next(Candidate s) {
+            int lastDictWordIndexIndex = s.dictWordIndexes.size() - 1;
+
+            int lastDictWordIndex
+                    = s.dictWordIndexes.get(lastDictWordIndexIndex);
+
+            if (lastDictWordIndex >= maxDictWordIndex) {
+                return null;
+            }
+
+            List<Integer> dictWordIndexes = new ArrayList<>(s.dictWordIndexes);
+
+            dictWordIndexes.set(lastDictWordIndexIndex, 1 + lastDictWordIndex);
+
+            return new Candidate(dictWordIndexes);
+        }
+
+        private void output(Candidate c) {
+            StringBuilder sentence = new StringBuilder();
+            String sep = null;
+
+            for (int dictWordIndex : c.dictWordIndexes) {
+                if (sep == null) {
+                    sep = " ";
+                } else {
+                    sentence.append(sep);
+                }
+
+                sentence.append(dictWords.get(dictWordIndex));
+            }
+
+            sentences.add(sentence.toString());
+        }
+
+        private void bt(Candidate c) {
+            if (reject(c)) {
+                return;
+            }
+
+            if (accept(c)) {
+                output(c);
+            }
+
+            Candidate s = first(c);
+
+            while (s != null) {
+                bt(s);
+                s = next(s);
+            }
+        }
+
+        public List<String> getSentences() {
+            this.bt(root());
+
+            return new ArrayList<>(sentences);
+        }
+    }
+
+    public List<String> wordBreak(String s, List<String> dictWords) {
+        Backtracker backtracker = new Backtracker(s, dictWords);
+
+        return backtracker.getSentences();
     }
 }
