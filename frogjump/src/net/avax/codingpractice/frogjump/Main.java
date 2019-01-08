d6946d6be88ee9184556d8e05c1eab8ef4f0dd64
Michael Krueger <5394172+interfrastic@users.noreply.github.com>
2019-01-07 17:00:13 -0800

    Commit skeleton frogjump project

---
+++ frogjump/src/net/avax/codingpractice/frogjump/Main.java
@@ -0,0 +1,12 @@
+package net.avax.codingpractice.frogjump;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int stones[] = {0, 1, 3, 5, 6, 8, 12, 17};
+
+        System.out.println(solution.canCross(stones));
+    }
+}
---
+++ frogjump/src/net/avax/codingpractice/frogjump/Solution.java
@@ -0,0 +1,13 @@
+package net.avax.codingpractice.frogjump;
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// LeetCode Frog Jump problem:
+//
+// https://leetcode.com/problems/frog-jump/
+
+class Solution {
+    public boolean canCross(int[] stones) {
+        return false;
+    }
+}
