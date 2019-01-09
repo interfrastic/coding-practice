1346a41e342a8581c95e304806ce0c96d3f8ed8a
Michael Krueger <5394172+interfrastic@users.noreply.github.com>
2019-01-09 10:07:20 -0800

    Record frogjump memoization solution #1
    39 / 39 test cases passed.
    Status: Accepted
    Runtime: 118 ms, faster than 27.29% of Java online submissions for Frog
    Jump.

--- frogjump/src/net/avax/codingpractice/frogjump/Solution.java
+++ frogjump/src/net/avax/codingpractice/frogjump/Solution.java
@@ -1,5 +1,12 @@
 package net.avax.codingpractice.frogjump;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+@SuppressWarnings("WeakerAccess")
+
 ////////////////////////////////////////////////////////////////////////////////
 //
 // LeetCode Frog Jump problem:
@@ -12,60 +19,83 @@ package net.avax.codingpractice.frogjump;
 // Status: Time Limit Exceeded
 //
 // https://leetcode.com/submissions/detail/199985323/
+//
+// Second attempt with memoization:
+//
+// 39 / 39 test cases passed.
+// Status: Accepted
+// Runtime: 118 ms, faster than 27.29% of Java online submissions for Frog Jump.
+//
+// https://leetcode.com/submissions/detail/200150064/
 
 class Solution {
-    private static boolean isStoneAt(int[] stones, int position) {
-        for (int stone : stones) {
-            if (stone == position) {
-                return true;
+    int finalPosition;
+    Set<Integer> stones = new HashSet<>();
+    Map<Integer, Map<Integer, Boolean>> startCache = new HashMap<>();
+
+    private boolean canCrossFrom(int start, int prevJump) {
+        if (startCache.containsKey(start)) {
+            Map<Integer, Boolean> prevJumpCache = startCache.get(start);
+
+            if (prevJumpCache.containsKey(prevJump)) {
+                return prevJumpCache.get(prevJump);
             }
         }
 
-        return false;
-    }
+        int shorterJump = prevJump - 1;
+        int longerJump = prevJump + 1;
 
-    private static boolean canCrossFrom(int[] stones, int start, int prevJump) {
-        int finalPosition = stones[stones.length - 1];
-        int shortJump = prevJump - 1;
-        int mediumJump = prevJump;
-        int longJump = prevJump + 1;
-
-        if ((shortJump > 0 && start + shortJump == finalPosition)
-                || mediumJump > 0 && start + mediumJump == finalPosition
-                || start + longJump == finalPosition) {
+        if ((shorterJump > 0 && start + shorterJump == finalPosition)
+                || prevJump > 0 && start + prevJump == finalPosition
+                || start + longerJump == finalPosition) {
             return true;
         }
 
         boolean canCross = false;
-        int jump = shortJump;
+        int jump = shorterJump;
         int landing = start + jump;
 
-        if (jump > 0 && isStoneAt(stones, landing)) {
-            canCross = canCrossFrom(stones, landing, jump);
+        if (jump > 0 && stones.contains(landing)) {
+            canCross = canCrossFrom(landing, jump);
         }
 
         if (!canCross) {
-            jump = mediumJump;
+            jump = prevJump;
             landing = start + jump;
 
-            if (jump > 0 && isStoneAt(stones, landing)) {
-                canCross = canCrossFrom(stones, landing, jump);
+            if (jump > 0 && stones.contains(landing)) {
+                canCross = canCrossFrom(landing, jump);
             }
         }
 
         if (!canCross) {
-            jump = longJump;
+            jump = longerJump;
             landing = start + jump;
 
-            if (isStoneAt(stones, landing)) {
-                canCross = canCrossFrom(stones, landing, jump);
+            if (stones.contains(landing)) {
+                canCross = canCrossFrom(landing, jump);
             }
         }
+
+        Map<Integer, Boolean> prevJumpCache
+                = startCache.computeIfAbsent(start, k -> new HashMap<>());
+
+        prevJumpCache.put(prevJump, canCross);
 
         return canCross;
     }
 
     public boolean canCross(int[] stones) {
-        return canCrossFrom(stones, 0, 0);
+        finalPosition = stones[stones.length - 1];
+
+        this.stones.clear();
+
+        for (int stone : stones) {
+            this.stones.add(stone);
+        }
+
+        startCache.clear();
+
+        return canCrossFrom(0, 0);
     }
 }
