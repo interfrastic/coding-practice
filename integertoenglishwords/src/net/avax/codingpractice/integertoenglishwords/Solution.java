c66b11101531aae996729d738d7af08a2cb466aa
Michael Krueger <5394172+interfrastic@users.noreply.github.com>
2019-01-16 14:38:05 -0800

    Commit skeleton integertoenglishwords project

---
+++ integertoenglishwords/src/net/avax/codingpractice/integertoenglishwords/Solution.java
@@ -0,0 +1,15 @@
+package net.avax.codingpractice.integertoenglishwords;
+
+@SuppressWarnings({"WeakerAccess"})
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// LeetCode Integer to English Words problem:
+//
+// https://leetcode.com/problems/integer-to-english-words/
+
+class Solution {
+    public String numberToWords(int num) {
+        // write your code here
+    }
+}
