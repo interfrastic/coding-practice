d81a55f660547edc7a1c2b7091e1244cc493be29
Michael Krueger <5394172+interfrastic@users.noreply.github.com>
2019-01-18 16:22:57 -0800

    Record slow, simple integertoenglishwords solution
    601 / 601 test cases passed.
    Status: Accepted
    Runtime: 8 ms, faster than 12.32% of Java online submissions for Integer
    to English Words.
    
    https://leetcode.com/submissions/detail/202133600/

--- integertoenglishwords/src/net/avax/codingpractice/integertoenglishwords/Main.java
+++ integertoenglishwords/src/net/avax/codingpractice/integertoenglishwords/Main.java
@@ -1,8 +1,23 @@
 package net.avax.codingpractice.integertoenglishwords;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        Solution solution = new Solution();
+
+        System.out.println(solution.numberToWords(0));
+        System.out.println(solution.numberToWords(1));
+        System.out.println(solution.numberToWords(10));
+        System.out.println(solution.numberToWords(11));
+        System.out.println(solution.numberToWords(100));
+        System.out.println(solution.numberToWords(101));
+        System.out.println(solution.numberToWords(999));
+        System.out.println(solution.numberToWords(1_000));
+        System.out.println(solution.numberToWords(1_000_000));
+        System.out.println(solution.numberToWords(1_001_000));
+        System.out.println(solution.numberToWords(1_000_000_000));
+        System.out.println(solution.numberToWords(1_000_000_001));
+        System.out.println(solution.numberToWords(1_000_001_000));
+        System.out.println(solution.numberToWords(1_001_000_000));
+        System.out.println(solution.numberToWords(Integer.MAX_VALUE));
     }
 }
--- integertoenglishwords/src/net/avax/codingpractice/integertoenglishwords/Solution.java
+++ integertoenglishwords/src/net/avax/codingpractice/integertoenglishwords/Solution.java
@@ -1,5 +1,8 @@
 package net.avax.codingpractice.integertoenglishwords;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SuppressWarnings({"WeakerAccess"})
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -7,9 +10,81 @@ package net.avax.codingpractice.integertoenglishwords;
 // LeetCode Integer to English Words problem:
 //
 // https://leetcode.com/problems/integer-to-english-words/
+//
+// First attempt: simple solution with some blocks of very similar code.
+//
+// 601 / 601 test cases passed.
+// Status: Accepted
+// Runtime: 8 ms, faster than 12.32% of Java online submissions for Integer to
+// English Words.
+//
+// https://leetcode.com/submissions/detail/202133600/
 
 class Solution {
+    private static int powerBase = 1_000;
+    private static int maxPowerValue = powerBase * powerBase * powerBase;
+    private static String[] powerValueNames = {"Thousand", "Million",
+            "Billion"};
+    private static String hundredName = "Hundred";
+    private static String[] multipleOfTenNames = {"Twenty", "Thirty", "Forty",
+            "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    private static String[] underTwentyNames = {"Zero", "One", "Two", "Three",
+            "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven",
+            "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen",
+            "Eighteen", "Nineteen"};
+
+    private List<String> numToWords(int num) {
+        List<String> words = new ArrayList<>();
+
+        if (num >= powerBase) {
+            int powerValue = maxPowerValue;
+            int powerValueNameIndex = powerValueNames.length;
+
+            do {
+                powerValueNameIndex--;
+
+                int powerValueNumPart = num / powerValue % powerBase;
+
+                if (powerValueNumPart != 0) {
+                    words.addAll(numToWords(powerValueNumPart));
+
+                    if (powerValueNameIndex >= 0) {
+                        String powerValueName
+                                = powerValueNames[powerValueNameIndex];
+
+                        words.add(powerValueName);
+                    }
+                }
+
+                powerValue /= powerBase;
+            } while (powerValue > 0);
+        } else if (num >= 100) {
+            int quotient = num / 100;
+            int remainder = num % 100;
+
+            words.addAll(numToWords(quotient));
+            words.add(hundredName);
+
+            if (remainder != 0) {
+                words.addAll(numToWords(remainder));
+            }
+        } else if (num >= 20) {
+            int quotient = num / 10;
+            int remainder = num % 10;
+
+            words.add(multipleOfTenNames[quotient - 2]);
+
+            if (remainder != 0) {
+                words.addAll(numToWords(remainder));
+            }
+        } else if (num >= 0) {
+            words.add(underTwentyNames[num]);
+        }
+
+        return words;
+    }
+
     public String numberToWords(int num) {
-        // write your code here
+        return String.join(" ", numToWords(num));
     }
 }
