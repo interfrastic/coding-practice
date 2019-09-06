004505ca132cd9be2825ecede921e78858c801f5
Michael Krueger <5394172+interfrastic@users.noreply.github.com>
2019-09-06 10:41:37 -0700

    Fix bugs in compareversionnumbers optimization

--- compareversionnumbers/src/net/avax/codingpractice/compareversionnumbers/Solution.java
+++ compareversionnumbers/src/net/avax/codingpractice/compareversionnumbers/Solution.java
@@ -23,62 +23,86 @@ package net.avax.codingpractice.compareversionnumbers;
 //
 // https://leetcode.com/submissions/detail/253992721/
 
+// Third attempt: Fix bugs in optimized version.
+//
+// 72 / 72 test cases passed.
+// Status: Accepted
+// Runtime: 0 ms, faster than 100.00% of Java online submissions for Compare
+// Version Numbers.
+// Memory Usage: 34 MB, less than 100.00% of Java online submissions for
+// Compare Version Numbers.
+//
+// https://leetcode.com/submissions/detail/258372095/
+
 import static java.lang.Character.isDigit;
 
 @SuppressWarnings("WeakerAccess")
 public class Solution {
+    private int compareNumbers(String str1, int beginIndex1, int endIndex1,
+                               String str2, int beginIndex2, int endIndex2) {
+        int result = 0;
+        int numLength1 = endIndex1 - beginIndex1;
+        int numLength2 = endIndex2 - beginIndex2;
+        int maxLength = Math.max(numLength1, numLength2);
+        int index = 0;
+
+        while (index < maxLength && result == 0) {
+            int index1 = endIndex1 - maxLength + index;
+            int index2 = endIndex2 - maxLength + index;
+            int ch1 = (index1 >= beginIndex1) ? str1.charAt(index1) : '0';
+            int ch2 = (index2 >= beginIndex2) ? str2.charAt(index2) : '0';
+
+            if (ch1 != ch2) {
+                result = (ch1 > ch2) ? 1 : -1;
+            }
+
+            index++;
+        }
+
+        return result;
+    }
+
     public int compareVersion(String version1, String version2) {
-        int indexLimit1 = version1.length();
-        int indexLimit2 = version2.length();
+        int result = 0;
         int index1 = 0;
         int index2 = 0;
 
-        while (index1 < indexLimit1 || index2 < indexLimit2) {
-            index1 = skipLeadingZeroes(version1, index1);
-            index2 = skipLeadingZeroes(version2, index2);
+        while ((index1 < version1.length() || index2 < version2.length())
+                && result == 0) {
+            int numBeginIndex1 = findNumBeginIndex(version1, index1);
+            int numEndIndex1 = findNumEndIndex(version1, numBeginIndex1);
+            int numBeginIndex2 = findNumBeginIndex(version2, index2);
+            int numEndIndex2 = findNumEndIndex(version2, numBeginIndex2);
 
-            char c1 = '\0';
-            char c2 = '\0';
-            char cc1;
-            char cc2;
+            result = compareNumbers(version1, numBeginIndex1, numEndIndex1,
+                    version2, numBeginIndex2, numEndIndex2);
 
-            do {
-                if (index1 < indexLimit1) {
-                    c1 = version1.charAt(index1);
-                    cc1 = (c1 == '.') ? '0' : c1;
-                    index1++;
-                } else {
-                    cc1 = '0';
-                }
-
-                if (index2 < indexLimit2) {
-                    c2 = version2.charAt(index2);
-                    cc2 = (c2 == '.') ? '0' : c2;
-                    index2++;
-                } else {
-                    cc2 = '0';
-                }
-
-                if (cc1 != cc2) {
-                    return (cc1 > cc2) ? 1 : -1;
-                }
-
-            } while (!((index1 == indexLimit1 || c1 == '.')
-                    && (index2 == indexLimit2 || c2 == '.')));
+            index1 = numEndIndex1;
+            index2 = numEndIndex2;
         }
 
-        return 0;
+        return result;
     }
 
-    private int skipLeadingZeroes(String number, int index) {
-        int indexLimit = number.length();
-        int newIndex = index;
+    private int findNumBeginIndex(String str, int beginIndex) {
+        int numBeginIndex = beginIndex;
 
-        while (index < indexLimit && number.charAt(index) == '0'
-                && ++index < indexLimit && isDigit(number.charAt(index))) {
-            newIndex = index;
+        while (numBeginIndex < str.length()
+                && !isDigit(str.charAt(numBeginIndex))) {
+            numBeginIndex++;
         }
 
-        return newIndex;
+        return numBeginIndex;
+    }
+
+    private int findNumEndIndex(String str, int beginIndex) {
+        int numEndIndex = beginIndex;
+
+        while (numEndIndex < str.length()
+                && isDigit(str.charAt(numEndIndex))) {
+            numEndIndex++;
+        }
+
+        return numEndIndex;
     }
 }
